d0e020e805f05c9afd028d64b3942f8ba58ba003
kwjinwoo <jung003327@gmail.com>
2021-09-08 23:27:53 +0900

    pre & auto

---
+++ structure data/task1/jinwoo/preprocessing.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+def make_train_set(df_):
+    df = df_.copy()
+
+    x_train = df.drop(['voted'], axis=1)
+    y_train = df['voted']
+
+    x_train['age_group'] = LabelEncoder().fit_transform(x_train['age_group'])
+    x_train['gender'] = LabelEncoder().fit_transform(x_train['gender'])
+
+    race_dummies = pd.get_dummies(x_train['race'], prefix='race_')
+    x_train = x_train.drop(['race'], axis=1)
+    x_train = pd.concat([x_train, race_dummies], axis=1)
+
+    religion_dummies = pd.get_dummies(x_train['religion'], prefix='religion_')
+    x_train = x_train.drop(['religion'], axis=1)
+    x_train = pd.concat([x_train, religion_dummies], axis=1)
+
+    return x_train, y_train, x_train.columns
+
