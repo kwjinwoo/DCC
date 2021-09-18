ff5342eb3b48b8eb94179c826e7db32e9ed0a979
kwjinwoo <jung003327@gmail.com>
2021-09-18 20:48:40 +0900

    fs

--- structure data/task2/jinwoo/preprocessing.py
+++ structure data/task2/jinwoo/preprocessing.py
@@ -4,12 +4,12 @@ import pandas as pd
 def main(df_):
     df = df_.copy()
 
+    # fill na
+    df = df.fillna('unknown')
+
     # drop FLAG_MOBIL
     df = df.drop(['FLAG_MOBIL'], axis=1)
 
-    # family_size 를 이산 변수로 변환
-    mask = df['family_size'] >= 5
-    df.loc[mask, 'family_size'] = 5
-    df['family_size'] = df['family_size'].astype('int')
-
+    # one-hot
+    df = 
     return df
