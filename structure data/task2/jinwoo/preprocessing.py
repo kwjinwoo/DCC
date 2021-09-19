a03ed0d8525f093ee6a5cd298ec3c1b562f01a03
kwjinwoo <jung003327@gmail.com>
2021-09-19 09:41:46 +0900

    feature_selection

--- structure data/task2/jinwoo/preprocessing.py
+++ structure data/task2/jinwoo/preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 def main(df_):
@@ -11,5 +12,15 @@ def main(df_):
     df = df.drop(['FLAG_MOBIL'], axis=1)
 
     # one-hot
-    df =
+    one_hot_list = ['income_type', 'edu_type', 'family_type', 'occyp_type', 'house_type']
+    for col in one_hot_list:
+        dummy = pd.get_dummies(df[col], prefix=col)
+        df = df.drop([col], axis=1)
+        df = pd.concat([df, dummy], axis=1)
+
+    # label
+    df['gender'] = LabelEncoder().fit_transform(df['gender'])
+    df['car'] = LabelEncoder().fit_transform(df['car'])
+    df['reality'] = LabelEncoder().fit_transform(df['reality'])
+
     return df
